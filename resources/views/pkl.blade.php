fbafabbb6ea7687b9ee8dcd42d2051269afbfb8c
Nanashaloes <ilhamputra1801@gmail.com>
2025-06-03 23:18:53 +0700

    Update pkl.blade.php

--- resources/views/pkl.blade.php
+++ resources/views/pkl.blade.php
@@ -1,8 +1,7 @@
 <x-layouts.app :title="__('PKL')">
     <div class="p-5 space-y-5 bg-gradient-to-b from-blue-100 to-blue-200 rounded-xl shadow-md">
         <div class="border border-blue-400 rounded-xl overflow-hidden bg-white shadow-lg">
-            <livewire:pkl.index />
+            <livewire:PKL.index />
         </div>
-        
     </div>
 </x-layouts.app>
